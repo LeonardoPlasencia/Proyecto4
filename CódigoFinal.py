d47c3eebe46e3ba049d661e46d8caa0bf3e9a99a
LeonardoPlasencia <106299475+LeonardoPlasencia@users.noreply.github.com>
2022-06-19 13:49:36 -0500

    Update CódigoFinal.py

--- CódigoFinal.py
+++ CódigoFinal.py
@@ -42,41 +42,6 @@ departamento, provincia y distrito.
 
 Fuente de datos: (https://www.datosabiertos.gob.pe/dataset/casos-positivos-por-covid-19-ministerio-de-salud-minsa)
 
-### 2. Origen de los datos
-El conjunto de dato se obtuvo del portal de datos abiertos del MINSA.
-| Variable        |	Descripción                                                                                         |
-| --------------- | --------------------------------------------------------------------------------------------------- |
-| FECHA_CORTE     |	Fecha de corte de información                                                                       |
-| UUID            |	ID de la persona como caso positivo de covid-19 confirmada con cualquier tipo de prueba.            |
-| UBIGEO          |	Código de Ubicación Geografica que denotan "DDppdd" (Departamento, provincia,distrito), fuente INEI |
-| DEPARTAMENTO    |	Departamento donde reside la persona confirmada como caso positivo de covid-19                      |
-| PROVINCIA       |	Provincia donde reside la persona confirmada como caso positivo de covid-19                         |
-| DISTRITO        |	Distrito donde reside la persona confirmada como caso positivo de covid-19                          |
-| METODODX        |	Metodo de laboratorio a la que es sometida una prueba de covid-19                                   |
-| EDAD            |	Edad de la persona confirmada como caso positivo de covid-19                                        |
-| SEXO            |	Sexo de la persona confirmada como caso positivo de covid-19                                        |
-| FECHA_RESULTADO |	Fecha del resultado de la prueba de covid-19                                                        |
-
-&nbsp;
-### 3. Herramientas utilizadas
-A continuación se listan los paqueyes de Python utilizaron durant el desarrollo del proyecto.
-```
-gdown==4.4.0
-numpy==1.22.4
-pandas==1.4.2
-pyecharts==1.9.1
-streamlit==1.10.0
-streamlit_echarts==0.4.0
-```
-
-### 4. Miembros del equipo
-- Isabel Muñoz
-- Enzo Baltazar
-- Joel Huillca
-- Stephany Samanez
-- Lucero de la Cruz
-- Leonardo Plasencia
-
 ### 5. Dashboard
 """)
 
